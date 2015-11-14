57949de79135be1c3c7a798548ef0704df80b8c2
Leda Duelo <leda.duelo@gmail.com>
2015-11-14 13:37:00 +0100

    He introducido un argumento

--- modeloconmasmotores.sh
+++ modeloconmasmotores.sh
@@ -2,4 +2,4 @@
 
 #Cálculo del modelo con más motores
 
-csvsort -d "^" -c nb_engines -r optd_aircraft.csv | head -2|csvcut -c model |tail -1 
+csvsort -d "^" -c nb_engines -r $1 | head -2|csvcut -c model |tail -1 
