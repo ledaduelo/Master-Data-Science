37d2013441ae16fe17ba538967b2097fb9218374
Leda Duelo <leda.duelo@gmail.com>
2015-11-14 13:13:32 +0100

    Update modeloconmasmotores.sh

--- modeloconmasmotores.sh
+++ modeloconmasmotores.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#esto es un comentario, lo anterior la cabecera
+#Cálculo del modelo con más motores
 
 csvsort -d "^" -c nb_engines -r optd_aircraft.csv | head -2|csvcut -c model |tail -1 
